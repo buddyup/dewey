3e75a4767ada52a240365fc243e639e761c11c84
skoczen <skoczen@gmail.com>
2013-10-11 15:20:00 -0700

    Structure for cross-platform commands in place.  Base command and hi command written.

--- dewey/commands/base.py
+++ dewey/commands/base.py
@@ -1,24 +1,59 @@
+VALID_PLATFORMS = ["Windows", "MacOSX", "Linux"]
 
 class DeweyCommand(object):
-    options = []
 
-    def pre_windows(self):
+    def set_platform(self, platform):
+        assert platform in VALID_PLATFORMS
+        self.platform = platform
+
+    def run_pre(self, *args, **kwargs):
+        if not self.platform:
+            raise OSError("echo 'Dewey doesn't know your operating system. Sorry!'")
+        if self.platform == "Windows":
+            self.pre_windows(*args, **kwargs)
+        elif self.platform == "MacOSX":
+            self.pre_macosx(*args, **kwargs)
+        elif self.platform == "Linux":
+            self.pre_unix(*args, **kwargs)
+        else:
+            raise OSError("echo 'Dewey doesn't know how to run pre for %s'" % self.platform)
+
+    def pre_windows(self, *args, **kwargs):
+        """Returns a string to execute on windows"""
         pass
 
-    def pre_macosx(self):
+    def pre_macosx(self, *args, **kwargs):
+        """Returns a string to execute on Mac OS X"""
         pass
 
-    def pre_unix(self):
+    def pre_unix(self, *args, **kwargs):
+        """Returns a string to execute on unix"""
         pass
 
-    def run_command(self):
+    def run_command(self, *args, **kwargs):
+        """Runs the body of the command (should not have current shell side effects.)"""
         pass
 
-    def post_windows(self):
+    def run_post(self, *args, **kwargs):
+        if not self.platform:
+            raise OSError("echo 'Dewey doesn't know your operating system. Sorry!'")
+        if self.platform == "Windows":
+            self.post_windows(*args, **kwargs)
+        elif self.platform == "MacOSX":
+            self.post_macosx(*args, **kwargs)
+        elif self.platform == "Linux":
+            self.post_unix(*args, **kwargs)
+        else:
+            raise OSError("echo 'Dewey doesn't know how to run post for %s'" % self.platform)
+
+    def post_windows(self, *args, **kwargs):
+        """Returns a string to execute on windows"""
         pass
 
-    def post_macosx(self):
+    def post_macosx(self, *args, **kwargs):
+        """Returns a string to execute on Mac OS X"""
         pass
 
-    def post_unix(self):
+    def post_unix(self, *args, **kwargs):
+        """Returns a string to execute on unix"""
         pass
--- dewey/hey_dewey.py
+++ dewey/hey_dewey.py
@@ -4,28 +4,59 @@
 Usage:
   dewey status
   dewey hi
-  dewey --pre
-  dewey --post
 
 Options:
   -h --help     Show this screen.
   --version     Show version.
-  --pre         Output the pre-command scripts.
-  --post        Output the post-command scripts.
+
+Hidden commands for scripts:
+  --pre         Output the pre-command scripts for execution.
+  --post        Output the post-command scripts for execution.
 
 """
+import sys
 from docopt import docopt
+
 from dewey import VERSION
 
+
 def main():
+    # Handle pre/post handlers
+    run_pre = False
+    run_post = False
+    if "--pre" in sys.argv:
+        run_pre = True
+        sys.argv.remove("--pre")
+    if "--post" in sys.argv:
+        run_post = True
+        sys.argv.remove("--post")
+
+    # Platform detection
+    platform = None
+    if sys.platform == "win32":
+        platform = "Windows"
+    elif sys.platform == "darwin":
+        platform = "MacOSX"
+    elif sys.platform == "linux2":
+        platform = "Linux"
+
     arguments = docopt(__doc__, version='Dewey %s' % VERSION)
 
-    # print(arguments)
-    if arguments["hi"]:
-        print "Oh, hello."
-
-    if arguments["status"]:
-        print "I'm fine, how are you?"
+    
+    for arg_name, value in arguments.iteritems():
+        if value == True:
+            try:
+                command_module = __import__("dewey.commands.%s" % arg_name, fromlist=['Command']) 
+                cmd = getattr(command_module, 'Command')()
+                cmd.set_platform(platform)
+                if run_pre:
+                    cmd.run_pre(**arguments)
+                elif run_post:
+                    cmd.run_post(**arguments)
+                else:
+                    cmd.run_command(**arguments)
+            except:
+                print "Unable to find a command module for %s" % arg_name
 
 if __name__ == '__main__':
     main()
