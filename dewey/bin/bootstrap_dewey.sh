df4312f8bec9dc46f3aaa441dcf1a6144dc2c3a4
skoczen <skoczen@gmail.com>
2013-10-25 12:36:06 -0700

    better namespacing of autocomplete function.

--- dewey/bin/bootstrap_dewey.sh
+++ dewey/bin/bootstrap_dewey.sh
@@ -1,13 +1,13 @@
-cd ~
+
 docopt-completion hey_dewey --manual-bash > /dev/null
-mv hey_dewey.sh ./bin/autocomplete.sh
+mv hey_dewey.sh .dewey_autocomplete.sh
 
-echo "" >> ./bin/autocomplete.sh
-echo "complete -F _hey_dewey hey_dewey" >> ./bin/autocomplete.sh
-echo "complete -F _hey_dewey dewey" >> ./bin/autocomplete.sh
-echo "complete -F _hey_dewey d" >> ./bin/autocomplete.sh
+echo "" >> .dewey_autocomplete.sh
+echo "complete -F _hey_dewey hey_dewey" >> .dewey_autocomplete.sh
+echo "complete -F _hey_dewey dewey" >> .dewey_autocomplete.sh
+echo "complete -F _hey_dewey d" >> .dewey_autocomplete.sh
 
-source ./bin/autocomplete.sh
+source .dewey_autocomplete.sh
 
 function _run_dewey() {
     `hey_dewey --pre $@`;
