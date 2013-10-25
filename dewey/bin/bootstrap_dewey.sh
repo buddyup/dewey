f705b4ce9f51b664c3274419fe7250ff6d711982
skoczen <skoczen@gmail.com>
2013-10-25 14:14:39 -0700

    GIve perms to autocomplete file.

--- dewey/bin/bootstrap_dewey.sh
+++ dewey/bin/bootstrap_dewey.sh
@@ -3,6 +3,7 @@ docopt-completion hey_dewey --manual-bash > /dev/null
 if [ ! -f ./hey_dewey.sh ]; then
     sudo docopt-completion hey_dewey --manual-bash > /dev/null
     sudo mv /etc/bash_completion.d/hey_dewey.sh ~/.dewey_autocomplete.sh
+    sudo chmod 755 ~/.dewey_autocomplete.sh
 else
     mv hey_dewey.sh .dewey_autocomplete.sh
 fi;
