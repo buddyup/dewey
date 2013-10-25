29ba04d63ed4986792cf0cc54730d2d4fac0e7c2
skoczen <skoczen@gmail.com>
2013-10-25 14:07:20 -0700

    Much simpler cross-platform approach.

--- dewey/bin/bootstrap_dewey.sh
+++ dewey/bin/bootstrap_dewey.sh
@@ -2,19 +2,15 @@ cd ~
 docopt-completion hey_dewey --manual-bash > /dev/null
 if [ ! -f ./hey_dewey.sh ]; then
     sudo docopt-completion hey_dewey --manual-bash > /dev/null
-    cd /etc/bash_completion.d/
-    sudo mv hey_dewey.sh .dewey_autocomplete.sh
-    echo "" | sudo tee .dewey_autocomplete.sh
-    echo "complete -F _hey_dewey hey_dewey" | sudo tee .dewey_autocomplete.sh
-    echo "complete -F _hey_dewey dewey" | sudo tee .dewey_autocomplete.sh
-    echo "complete -F _hey_dewey d" | sudo tee .dewey_autocomplete.sh
+    sudo mv /etc/bash_completion.d/hey_dewey.sh ~/.dewey_autocomplete.sh
 else
     mv hey_dewey.sh .dewey_autocomplete.sh
-    echo "" >> .dewey_autocomplete.sh
-    echo "complete -F _hey_dewey hey_dewey" >> .dewey_autocomplete.sh
-    echo "complete -F _hey_dewey dewey" >> .dewey_autocomplete.sh
-    echo "complete -F _hey_dewey d" >> .dewey_autocomplete.sh
 fi;
+
+echo "" >> ~/.dewey_autocomplete.sh
+echo "complete -F _hey_dewey hey_dewey" >> ~/.dewey_autocomplete.sh
+echo "complete -F _hey_dewey dewey" >> .dewey_autocomplete.sh
+echo "complete -F _hey_dewey d" >> .dewey_autocomplete.sh
 
 source .dewey_autocomplete.sh
 
