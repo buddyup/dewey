e25f16050dd14a6c5697548e19440a96759fa37c
Steven Skoczen <steven@inkandfeet.com>
2016-02-19 12:53:31 +0200

    Flips to post-default

--- dewey/commands/bootstrap.py
+++ dewey/commands/bootstrap.py
@@ -9,17 +9,17 @@ from dewey.util import suppress_stdout_stderr
 class Command(DeweyCommand):
 
     def pre_default(self, *args, **kwargs):
-        return "whoami;  cd ~/buddyup/core.git ; docker-osx-dev -e .git -e bower_components -e node_modules -e source"
+        pass
 
     def run_command(self, *args, **kwargs):
-        # Base OSX Dev
-        output = subprocess.check_output("docker-osx-dev", shell=True, )
+        # # Base OSX Dev
+        # output = subprocess.check_output("docker-osx-dev", shell=True, )
 
         # Dev DNS
         output = subprocess.check_output("docker run -d --name devdns -p 53:53/udp -v /var/run/docker.sock:/var/run/docker.sock ruudud/devdns --restart always", shell=True, )
 
         # Old image cleanup
-        output = subprocess.check_output("docker run -d --name cleanup -v /var/run/docker.sock:/var/run/docker.sock:rw -v /var/lib/docker:/var/lib/docker:rw meltwater/docker-cleanup:latest ", shell=True, )
+        output = subprocess.check_output("docker run -d --name cleanup --restart always -v /var/run/docker.sock:/var/run/docker.sock:rw -v /var/lib/docker:/var/lib/docker:rw meltwater/docker-cleanup:latest ", shell=True, )
 
         # Get the latest boot2docker
         output = subprocess.check_output("docker-machine upgrade default", shell=True, )
@@ -27,4 +27,4 @@ class Command(DeweyCommand):
         print("Ready for development")
 
     def post_default(self, *args, **kwargs):
-        pass
+        return "yes | docker-osx-dev -e .git -e bower_components -e node_modules -e source"
