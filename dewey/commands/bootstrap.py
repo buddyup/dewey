49728da9ce0124a29369d5c82f43ab28b955fb24
Steven Skoczen <steven@inkandfeet.com>
2016-02-19 13:41:19 +0200

    Handle already running more cleanly.

--- dewey/commands/bootstrap.py
+++ dewey/commands/bootstrap.py
@@ -14,18 +14,21 @@ class Command(DeweyCommand):
     def run_command(self, *args, **kwargs):
         # # Base OSX Dev
         # output = subprocess.check_output("docker-osx-dev", shell=True, )
+        running = subprocess.check_output("docker ps", shell=True)
 
         # Dev DNS
-        try:
-            output = subprocess.check_output("docker run -d --name devdns -p 53:53/udp -v /var/run/docker.sock:/var/run/docker.sock ruudud/devdns --restart always", shell=True, )
-        except:
-            output = subprocess.check_output("docker run ruudud/devdns -d -p 53:53/udp -v /var/run/docker.sock:/var/run/docker.sock --restart always", shell=True, )
+        if "ruudud/devdns" not in running:
+            try:
+                output = subprocess.check_output("docker run -d --name devdns -p 53:53/udp -v /var/run/docker.sock:/var/run/docker.sock ruudud/devdns --restart always", shell=True, )
+            except:
+                output = subprocess.check_output("docker run ruudud/devdns -d -p 53:53/udp -v /var/run/docker.sock:/var/run/docker.sock --restart always", shell=True, )
 
-        # Old image cleanup
-        try:
-            output = subprocess.check_output("docker run -d --name cleanup --restart always -v /var/run/docker.sock:/var/run/docker.sock:rw -v /var/lib/docker:/var/lib/docker:rw meltwater/docker-cleanup:latest ", shell=True, )
-        except:
-            output = subprocess.check_output("docker run meltwater/docker-cleanup:latest -d --restart always -v /var/run/docker.sock:/var/run/docker.sock:rw -v /var/lib/docker:/var/lib/docker:rw", shell=True, )        
+        if "docker-cleanup" not in running:
+            # Old image cleanup
+            try:
+                output = subprocess.check_output("docker run -d --name cleanup --restart always -v /var/run/docker.sock:/var/run/docker.sock:rw -v /var/lib/docker:/var/lib/docker:rw meltwater/docker-cleanup:latest ", shell=True, )
+            except:
+                output = subprocess.check_output("docker run meltwater/docker-cleanup:latest -d --restart always -v /var/run/docker.sock:/var/run/docker.sock:rw -v /var/lib/docker:/var/lib/docker:rw", shell=True, )        
 
         # Get the latest boot2docker
         output = subprocess.check_call("docker-machine upgrade default", shell=True, )
