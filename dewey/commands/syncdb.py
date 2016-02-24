f17284acc460709dc6daa0acb34e9525bd2168c9
Steven Skoczen <steven@inkandfeet.com>
2016-02-24 14:47:49 +0200

    Adds quotes

--- dewey/commands/syncdb.py
+++ dewey/commands/syncdb.py
@@ -9,7 +9,7 @@ from dewey.util import suppress_stdout_stderr
 class Command(DeweyCommand):
 
     def pre_default(self, *args, **kwargs):
-        return "docker-compose run db bash -c 'createdb -h db -U postgres buddyup'"
+        return "docker-compose run db \"bash -c 'createdb -h db -U postgres buddyup'\""
 
     def run_command(self, *args, **kwargs):
         pass
