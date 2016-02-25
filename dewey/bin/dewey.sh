7d2cf126bec3d8903a891fa5af41eab56ee1d2ab
Steven Skoczen <steven@inkandfeet.com>
2016-02-25 14:26:51 +0200

    I almost can't.

--- dewey/bin/dewey.sh
+++ dewey/bin/dewey.sh
@@ -1,3 +1,3 @@
-source <(hey_dewey --pre $@)
+hey_dewey --pre $@ > /tmp/dewey_pre && source /tmp/dewey_pre && rm /tmp/dewey_pre
 hey_dewey $@
-source <(hey_dewey --post $@)
+hey_dewey --post $@ > /tmp/dewey_post && source /tmp/dewey_post && rm /tmp/dewey_post
