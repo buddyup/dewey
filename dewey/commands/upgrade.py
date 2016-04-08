f01b529fe91e4073c24f7ad703e5375bad9445b0
Steven Skoczen <steven@inkandfeet.com>
2016-04-08 14:48:09 +0700

    Spent 3 minutes making something prettier.  Ahhhh. :)

--- dewey/commands/upgrade.py
+++ dewey/commands/upgrade.py
@@ -1,13 +1,17 @@
+import subprocess
 from .base import DeweyCommand
 
 
 class Command(DeweyCommand):
 
     def pre_default(self, *args, **kwargs):
-        return "pip install git+https://git@github.com/buddyup/dewey.git#egg=dewey --upgrade"
+        return "echo 'Upgrading dewey...'"
 
     def run_command(self, *args, **kwargs):
-        print("Upgraded.")
+        try:
+            output = subprocess.check_output("pip install git+https://git@github.com/buddyup/dewey.git#egg=dewey --upgrade", shell=True, )
+        except subprocess.CalledProcessError as grepexc:                                                                                                   
+            print("Error upgrading dewey. \n%s" % (grepexc.output,))
 
     def post_default(self, *args, **kwargs):
         pass
