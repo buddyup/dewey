e0d933b042ba3489a7e9e2eaaca478d48812f8a5
Steven Skoczen <steven@inkandfeet.com>
2016-04-08 15:27:14 +0700

    Catch gracefully

--- dewey/commands/up.py
+++ dewey/commands/up.py
@@ -14,16 +14,20 @@ class Command(DeweyCommand):
         pass
 
     def run_command(self, *args, **kwargs):
-        ps = subprocess.Popen(
-            "npm run watch",
-            close_fds=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
-        while ps.poll() is None:
-            out = ps.stdout.read(1)
-            sys.stdout.write(out)
-            sys.stdout.flush()
-            if "bundle is now VALID" in out:
-                subprocess.call("open http://localhost:8080", shell=True,)
+        try:
+            ps = subprocess.Popen(
+                "npm run watch",
+                close_fds=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
+            while ps.poll() is None:
+                out = ps.stdout.read(1)
+                sys.stdout.write(out)
+                sys.stdout.flush()
+                if "bundle is now VALID" in out:
+                    print "Bundling done.  Launching browser."
+                    subprocess.check_output("open http://localhost:8080", shell=True,)
+        except KeyboardInterrupt:
+            print "Shutting down."
 
     def post_default(self, *args, **kwargs):
         pass
