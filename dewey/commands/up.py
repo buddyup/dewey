894f093f7d0d298c95b6d94505f8982dc938ea8f
Steven Skoczen <steven@inkandfeet.com>
2016-04-08 15:21:43 +0700

    Catch & open.

--- dewey/commands/up.py
+++ dewey/commands/up.py
@@ -13,7 +13,14 @@ class Command(DeweyCommand):
         pass
 
     def run_command(self, *args, **kwargs):
-        output = subprocess.call("sleep 5; open http://localhost:8080", shell=True,)
+        ps = subprocess.Popen(
+            "npm run watch",
+            close_fds=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        while ps.poll() is None:
+            out = ps.stdout.read(1)
+            if "bundle is now VALID." in out:
+                subprocess.call("open http://localhost:8080", shell=True,)
 
     def post_default(self, *args, **kwargs):
-        return "npm run watch"
+        pass
