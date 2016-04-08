daac70bcfa7cb590adec75ad95eb391bf06f97fa
Steven Skoczen <steven@inkandfeet.com>
2016-04-08 15:45:14 +0700

    Keeps it local

--- dewey/commands/android.py
+++ dewey/commands/android.py
@@ -5,12 +5,10 @@ from .base import DeweyCommand
 from dewey.util import suppress_stdout_stderr
 
 
+run_android = True
+
 
 class Command(DeweyCommand):
-
-    def __init__(self, *args, **kwargs):
-        super(Command, self).__init__(*args, **kwargs)
-        self.run_android = True
 
     def pre_default(self, *args, **kwargs):
         pass
@@ -19,9 +17,9 @@ class Command(DeweyCommand):
         output = subprocess.check_output("adb devices -l", shell=True, )
         if "device usb" not in output:
             print "No android device connected via USB or in Genymotion.  Stopping."
-            self.run_android = False
+            run_android = False
 
 
     def post_default(self, *args, **kwargs):
-        if self.run_android:
+        if run_android:
             return "npm run android"
