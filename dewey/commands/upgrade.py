710c35964b21fd2e91d025335fa4cfaf1e958369
Steven Skoczen <steven@inkandfeet.com>
2016-04-08 15:10:05 +0700

    Nicer upgrade

--- dewey/commands/upgrade.py
+++ dewey/commands/upgrade.py
@@ -5,15 +5,16 @@ from dewey.util import suppress_stdout_stderr
 class Command(DeweyCommand):
 
     def pre_default(self, *args, **kwargs):
-        return "echo 'Upgrading dewey...'"
+        pass
 
     def run_command(self, *args, **kwargs):
+        print("Upgrading dewey...",)
         with suppress_stdout_stderr():
             try:
                 output = subprocess.check_output("pip install git+https://git@github.com/buddyup/dewey.git#egg=dewey --upgrade", shell=True, )
-                print("Complete.")
+                print(" complete.")
             except subprocess.CalledProcessError as grepexc:                                                                                                   
-                print("Error upgrading dewey. \n%s" % (grepexc.output,))
+                print("problem!\nError upgrading dewey. \n%s" % (grepexc.output,))
 
 
     def post_default(self, *args, **kwargs):
