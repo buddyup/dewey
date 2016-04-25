b931a405b1a1a07325ee4aed90de20abbfdf101d
Steven Skoczen <steven@inkandfeet.com>
2016-04-25 12:01:17 +0700

    Comments out old

--- dewey/commands/up.py
+++ dewey/commands/up.py
@@ -12,7 +12,6 @@ class Command(DeweyCommand):
     def pre_default(self, *args, **kwargs):
         pass
 
-
     def run_command(self, *args, **kwargs):
         try:
             ps = subprocess.Popen(
@@ -27,22 +26,21 @@ class Command(DeweyCommand):
         except KeyboardInterrupt:
             print "\n\nShutting down."
 
-
-    def run_command_oliver(self, *args, **kwargs):
-        try:
-            ps = subprocess.Popen(
-                "npm run watch",
-                close_fds=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-            )
-            while ps.poll() is None:
-                line = ps.stdout.readline()
-                if "[BS] Serving files from: frontends/groundcontrol/build/web" in line:
-                    print "Bootstrapping done.  Launching browser."
-                    subprocess.check_output("open http://localhost:8111", shell=True,)
-                sys.stdout.write(line)
-                sys.stdout.flush()
-        except KeyboardInterrupt:
-            print "\n\nShutting down."
+    # def run_command_oliver(self, *args, **kwargs):
+    #     try:
+    #         ps = subprocess.Popen(
+    #             "npm run watch",
+    #             close_fds=True, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    #         )
+    #         while ps.poll() is None:
+    #             line = ps.stdout.readline()
+    #             if "[BS] Serving files from: frontends/groundcontrol/build/web" in line:
+    #                 print "Bootstrapping done.  Launching browser."
+    #                 subprocess.check_output("open http://localhost:8111", shell=True,)
+    #             sys.stdout.write(line)
+    #             sys.stdout.flush()
+    #     except KeyboardInterrupt:
+    #         print "\n\nShutting down."
 
     def post_default(self, *args, **kwargs):
         pass
