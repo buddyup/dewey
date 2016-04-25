e24c851d5727d790f94ea065c202e6c859cd7537
Steven Skoczen <steven@inkandfeet.com>
2016-04-25 12:10:52 +0700

    Adds some nice output.

--- dewey/commands/reset.py
+++ dewey/commands/reset.py
@@ -8,17 +8,32 @@ from dewey.util import suppress_stdout_stderr
 
 class Command(DeweyCommand):
 
+    def print_section(self, message):
+        print("=".join(range(0, len(message))))
+        print(message)
+        print("=".join(range(0, len(message))))
+
+
     def pre_default(self, *args, **kwargs):
         pass
 
     def run_command(self, *args, **kwargs):
+        print("Resetting libraries and native components")
+        print_section("Clearing NPM and Bower")
         subprocess.call("rm -rf node_modules", cwd="app", shell=True)
         subprocess.call("rm -rf bower_components", cwd="app", shell=True)
         subprocess.call("npm cache clean", cwd="app", shell=True)
+        print_section("Installing NPM Libraries")
         subprocess.call("npm install", cwd="app", shell=True)
+        print_section("Installing Bower Libraries")
         subprocess.call("bower install", cwd="app", shell=True)
+        print_section("Installing Python Libraries")
+        subprocess.call("pip install requirements.unstable.txt --save", shell=True)
+        print_section("Resetting Ionic App")
         subprocess.call("ionic state reset", cwd="app/native/ionic", shell=True)
+        print_section("Rebuilding Ionic Splash Resources")
         subprocess.call("ionic resources", cwd="app/native/ionic", shell=True)
+        print("Reset complete.")
 
     def post_default(self, *args, **kwargs):
         pass
