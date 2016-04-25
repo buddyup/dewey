640a5a4eca040d5611fc4827ec75e1345e2a62ec
Steven Skoczen <steven@inkandfeet.com>
2016-04-25 11:04:03 +0700

    updates workon

--- dewey/commands/workon.py
+++ dewey/commands/workon.py
@@ -4,10 +4,10 @@ class Command(DeweyCommand):
 
     def pre_default(self, *args, **kwargs):
         # return "cd ~/buddyup/oliver"
-        return "cd ~/buddyup/core"
+        return "workon buddyup-core"
 
     def run_command(self, *args, **kwargs):
         pass
 
     def post_default(self, *args, **kwargs):
-        pass
+        return "cd ~/buddyup/core"
