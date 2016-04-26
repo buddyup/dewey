80bb7cbc0c9be78d5e005f625e4d2cbc29076099
Steven Skoczen <steven@inkandfeet.com>
2016-04-26 13:02:45 +0700

    Goes a little harder core with the reset, includes python-land

--- dewey/commands/reset.py
+++ dewey/commands/reset.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from clint.textui import puts, indent, colored
 
@@ -19,22 +20,42 @@ class Command(DeweyCommand):
 
     def run_command(self, *args, **kwargs):
         print("Resetting libraries and native components")
+
+        self.print_section("Updating global libs")
+        subprocess.call("npm install -g ionic", shell=True)
+
         self.print_section("Clearing NPM and Bower")
         subprocess.call("rm -rf node_modules", cwd="app", shell=True)
         subprocess.call("rm -rf bower_components", cwd="app", shell=True)
         subprocess.call("npm cache clean", cwd="app", shell=True)
+
         self.print_section("Installing NPM Libraries")
         subprocess.call("npm install", cwd="app", shell=True)
+
         self.print_section("Installing Bower Libraries")
         subprocess.call("bower install", cwd="app", shell=True)
+
+        self.print_section("Backing up postactivate")
+        if not os.path.isfile("postactivate"):
+            subprocess.call("cp ~/.virtualenvs/buddyup-core/bin/postactivate postactivate", shell=True)
+
+        self.print_section("Clearing virtualenv")
+        subprocess.call("rmvirtualenv buddyup-core", shell=True)
+        subprocess.call("mkvirtualenv buddyup-core -p `which python3`", shell=True)
+
+        self.print_section("Setting up postactivate")
+        subprocess.call("rm ~/.virtualenvs/buddyup-core/bin/postactivate", shell=True)
+        subprocess.call("ln -s postactivate ~/.virtualenvs/buddyup-core/bin/postactivate", shell=True)
+
         self.print_section("Installing Python Libraries")
-        subprocess.call("pip install requirements.unstable.txt", shell=True)
-        subprocess.call("pip freeze -r requirements.unstable.txt > requirements.txt", shell=True)
+        subprocess.call("workon buddyup-core; pip install requirements.txt", shell=True)
+
         self.print_section("Resetting Ionic App")
         subprocess.call("ionic state reset", cwd="app/native/ionic", shell=True)
+
         self.print_section("Rebuilding Ionic Splash Resources")
         subprocess.call("ionic resources", cwd="app/native/ionic", shell=True)
         print("Reset complete.")
 
     def post_default(self, *args, **kwargs):
-        pass
+        return "workon buddyup-core"
